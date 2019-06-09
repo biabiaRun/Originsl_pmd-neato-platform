52396f5090cc51e86dac30789328d5ee720f2df3
Alexander Strauch <a.strauch@pmdtec.com>
2019-06-09 20:08:27 +0200

    Removed tests that were specific to the other module

--- platform/test/src/TestPlatform_Specific.cpp
+++ platform/test/src/TestPlatform_Specific.cpp
@@ -15,37 +15,3 @@
 using namespace royale;
 using namespace platform;
 
-TEST_F (CameraDeviceFixture, ValidateUseCases)
-{
-    initCamera();
-
-    Vector<String> supportedUseCases;
-
-    auto status = camera->getUseCases (supportedUseCases);
-    ASSERT_EQ (status, CameraStatus::SUCCESS);
-
-    ASSERT_EQ (supportedUseCases.size(), 5u);
-
-    EXPECT_STREQ (supportedUseCases.at (0).c_str(), "MODE_5_25FPS");
-    EXPECT_STREQ (supportedUseCases.at (1).c_str(), "MODE_5_30FPS");
-    EXPECT_STREQ (supportedUseCases.at (2).c_str(), "MODE_5_45FPS");
-    EXPECT_STREQ (supportedUseCases.at (3).c_str(), "MODE_5_50FPS");
-    EXPECT_STREQ (supportedUseCases.at (4).c_str(), "MODE_5_60FPS");
-}
-
-TEST_F (CameraDeviceFixture, CheckFPS)
-{
-    camera->setCallbackData (CallbackData::Raw);
-
-    initCamera();
-
-#ifdef ROYALE_ENABLE_PLATFORM_CODE
-    runUseCaseAndCheckFPS ("MODE_5_25FPS", 25);
-    runUseCaseAndCheckFPS ("MODE_5_30FPS", 30);
-    runUseCaseAndCheckFPS ("MODE_5_45FPS", 45);
-    runUseCaseAndCheckFPS ("MODE_5_50FPS", 50);
-    runUseCaseAndCheckFPS ("MODE_5_60FPS", 60);
-#else
-    runUseCaseAndCheckFPS ("Fast_Acquisition", 45);
-#endif
-}
