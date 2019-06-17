e4d55de5167603d699d9ec5f1c5916fd80a9c605
Dmitry Konyshev <probables@emcraft.com>
2019-06-17 16:00:45 +0300

    3574 add 80 MHz use cases

--- platform/sampleRetrieveData/src/sampleRetrieveData.cpp
+++ platform/sampleRetrieveData/src/sampleRetrieveData.cpp
@@ -76,7 +76,7 @@ int main (int argc, char **argv)
     }
 
     // choose a use case
-    auto selectedUseCaseIdx = 13u;
+    auto selectedUseCaseIdx = 12u;
 
     // set an operation mode
     cout << "Set use case start" << std::endl;
